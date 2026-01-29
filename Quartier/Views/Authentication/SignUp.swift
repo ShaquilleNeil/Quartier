e908c1cd608a00a741089679fa2cba079b2b721f
shaquille neil <shaquille-36@hotmail.com>
2026-01-29 09:46:22 -0500

    Updated sign up

--- Quartier/Views/Authentication/SignUp.swift
+++ Quartier/Views/Authentication/SignUp.swift
@@ -9,7 +9,42 @@ import SwiftUI
 
 struct SignUp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("sign up")
+            
+            Form{
+                Section(header: Text("Sign Up")){
+                    TextField("Email", text: .constant(""))
+                    SecureField("Password", text: .constant(""))
+                    Button(action:{})
+                    {
+                        
+                        Text("Sign Up")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.orange)
+                            .cornerRadius(10)
+                        
+                    }
+                    
+                }
+                
+                Section(header: Text("Already have an account?")){
+                    Button(action:{}){
+                        Text("Log In")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(10)
+                        
+                    }
+                }
+            }
+        }
     }
 }
 
