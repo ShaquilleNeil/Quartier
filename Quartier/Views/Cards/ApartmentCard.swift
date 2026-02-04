677d32dee7f2da55e42c6316705f0d4206beed41
shaquille neil <shaquille-36@hotmail.com>
2026-02-03 22:02:35 -0500

    Tenant Home, Apartment Card and profile(70%)

---
+++ Quartier/Views/Cards/ApartmentCard.swift
@@ -0,0 +1,18 @@
+//
+//  ApartmentCard.swift
+//  Quartier
+//
+//  Created by Shaquille O Neil on 2026-02-03.
+//
+
+import SwiftUI
+
+struct ApartmentCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApartmentCard()
+}
